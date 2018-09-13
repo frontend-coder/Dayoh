ff875e1262dc36ee0aedeeede31eede5a5cb6bd2
DESKTOP-DD2BR4M\Front-End Developer <frontendercode@gmail.com>
2018-09-14 00:24:31 +0300

    v.0.18
    Сборка шаблона переведена на Gulp, исправлена верстка шапки

---
+++ app/libs/jquery/common.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	$("body").niceScroll({
+horizrailenabled:false
+});
+$(function() {
+	$("#phone_key").mask("+7(000)000-00-00", {placeholder: "+7(___)___-__-__",clearIfNotMatch: true});
+	$("#phone_header").mask("+7(000)000-00-00", {placeholder: "+7(___)___-__-__",clearIfNotMatch: true});
+});
+
+// всплывающие окна обратной связи позвонить мне
+$("a[href='#call-back']").magnificPopup ({
+	mainClass:'mfp-fade',
+	removalDelay:400,
+	type:'inline'
+});
+
+/*чтобы в формах был индивидуальный заголовок */
+$("a[href='#call-back']").click(function(){
+	var dataForm = $(this).data("form");
+	var dataText = $(this).data("text");
+	$(".forms-call h4").text(dataText);
+	$(".forms-call [name=admin-data]").val(dataForm);
+});
+
+
+//Ajax push mesege http://api.jquery.com/jquery.ajax/
+
+$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(".forms-calldecor .success").addClass("active");
+			setTimeout(function() {
+				// Done Functions
+				$(".forms-calldecor .success").removeClass("active");
+				th.trigger("reset");
+				$.magnificPopup.close();
+			}, 3000);
+		});
+		return false;
+	});
+//castom code
+
+
+
+});
